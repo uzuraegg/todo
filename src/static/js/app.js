9449a8821eefa514108db5cfe2c6faab846c264a
diesen <diesen.aruzu65@gmail.com>
2020-12-28 23:31:40 +0900

    change text

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,7 +1,7 @@
 function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
-        <h1>Buttons</h1>
+        <h1>Todo App</h1>
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
